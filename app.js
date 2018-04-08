78adf610d089e626090e9f91b25027c474e2cd79
Nishant Kumar Jaiswal <nishant.soft04@gmail.com>
2018-04-08 15:53:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get("/", function(req, res){
 });
 
 io.on("connection", function(socket){
-	socket.on("stream-1", function(image){
+	socket.on("stream1", function(image){
 		socket.broadcast.emit("stream", image);
 	});
 });
